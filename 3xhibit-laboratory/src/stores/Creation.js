ca7ea47fcbd2a3f95033a443c3493a87ce624ac3
polarzero <0xpolarzero@gmail.com>
2022-12-07 00:33:55 +0100

    Add random color & pointSize

--- 3xhibit-laboratory/src/stores/Creation.js
+++ 3xhibit-laboratory/src/stores/Creation.js
@@ -1,6 +1,9 @@
+import { getRandomColor } from '../utils/getRandomColor';
 import create from 'zustand';
 
 export default create((set) => ({
-  color: 'ivory',
+  color: getRandomColor(),
+  pointSize: 3,
   setColor: (color) => set({ color }),
+  setPointSize: (pointSize) => set({ pointSize }),
 }));
