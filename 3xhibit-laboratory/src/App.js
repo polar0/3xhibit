f6e0b320206d5ebc37502c2b657c3f971a2dcc2e
polarzero <0xpolarzero@gmail.com>
2022-12-07 00:33:46 +0100

    Add StrictMode

--- 3xhibit-laboratory/src/App.js
+++ 3xhibit-laboratory/src/App.js
@@ -1,22 +1,25 @@
 import World from './World';
 import { Canvas } from '@react-three/fiber';
 import Interface from './Interface';
+import { StrictMode } from 'react';
 
 export const App = () => {
   return (
     <>
-      <Canvas
-        shadows
-        camera={{
-          fov: 45,
-          near: 0.1,
-          far: 200,
-          position: [0, 2, 8],
-        }}
-      >
-        <World />
-      </Canvas>
-      <Interface />
+      <StrictMode>
+        <Canvas
+          shadows
+          camera={{
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [0, 2, 8],
+          }}
+        >
+          <World />
+        </Canvas>
+        <Interface />
+      </StrictMode>
     </>
   );
 };
